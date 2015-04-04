ee2b29d5ccd66816533c8796e0c86993acb87642
WebGuru79 <indusmd79@gmail.com>
2015-04-04 19:11:11 -0400

    First commit

---
+++ UCT/Models/Version.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using UCT.Models ;
+
+namespace UCT.Models
+{
+    public class Version
+    {       
+        public int VersionID { get; set; }
+        public int ProgramID {get; set;}
+        public IEnumerable<Program> UserPrograms { get; set; }
+    }
+}
---
+++ www/Models/Version.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UCT.Models
+{
+    public class Version
+    {
+        public int VersionID { get; set; }
+    }
+}
